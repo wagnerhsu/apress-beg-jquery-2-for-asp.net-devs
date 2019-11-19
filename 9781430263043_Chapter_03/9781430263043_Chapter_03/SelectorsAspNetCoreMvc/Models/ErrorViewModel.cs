4a15e12d81daf07bbb1d351acf1a6b08a2d7a7d1
Wagner Xu <wagnerhsu@hotmail.com>
2019-11-19 22:16:24 +0800

    Add SelectorAspNetCoreMvc

---
+++ 9781430263043_Chapter_03/9781430263043_Chapter_03/SelectorsAspNetCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SelectorsAspNetCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
